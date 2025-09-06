85825547092ec43390dd62b94fc2b4b0700f039d
HimanshuDahal <himanshudahal123@gmail.com>
2025-09-06 02:17:33 -0700

    Completed

--- m3-wk1-d2-demo/exp_demo2.js
+++ m3-wk1-d2-demo/exp_demo2.js
@@ -1,8 +1,8 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-
+app.get("/hello", function (req, res) {
+  res.send("Hello world!");
+});
 
 app.listen(3000);
-
-
